f91db3f56eefb362b9455aa0d04d681cb17ad423
Mingfu Shao <mingfu.shao@cs.cmu.edu>
2017-02-16 15:51:04 -0500

    finish adding variables to ILP

--- ilpsolver/src/ilpsolver.cc
+++ ilpsolver/src/ilpsolver.cc
@@ -18,6 +18,16 @@ ilpsolver::~ilpsolver()
 	if(env != NULL) delete env;
 }
 
+int ilpsolver::solve()
+{
+	add_amino_acid_variables();
+	add_lower_endpoints_variables();
+	add_upper_endpoints_variables();
+	add_range_variables();
+	add_error_variables();
+	return 0;
+}
+
 int ilpsolver::read_alphabet(const string &file)
 {
 	ifstream fin(file.c_str());
@@ -34,9 +44,13 @@ int ilpsolver::read_alphabet(const string &file)
 	{
 		stringstream sstr(line);
 		sstr >> a >> w;
+		aa_list.push_back(a);
+		aa_mass.push_back(w);
+		/*
 		string s(a);
 		if(aa2m.find(s) == aa2m.end()) aa2m.insert(PSD(s, w));
 		else aa2m[s] = w;
+		*/
 	}
 	return 0;
 }
@@ -55,7 +69,7 @@ int ilpsolver::read_spectrum(const string &file)
 	{
 		if(line == "") continue;
 		stringstream sstr(line);
-		sstr >> num_aa;
+		sstr >> slots;
 		break;
 	}
 
@@ -71,15 +85,98 @@ int ilpsolver::read_spectrum(const string &file)
 	return 0;
 }
 
+int ilpsolver::add_amino_acid_variables()
+{
+	xvars.clear();
+	xvars.resize(slots);
+	for(int i = 0; i < slots; i++)
+	{
+		for(int k = 0; k < aa_list.size(); k++)
+		{
+			GRBVar var = model->addVar(0, 1, 0, GRB_BINARY);
+			xvars[i].push_back(var);
+		}
+	}
+	model->update();
+	return 0;
+}
+
+int ilpsolver::add_lower_endpoints_variables()
+{
+	lvars.clear();
+	lvars.resize(spectrum.size());
+	for(int i = 0; i < spectrum.size(); i++)
+	{
+		for(int k = 0; k < aa_list.size(); k++)
+		{
+			GRBVar var = model->addVar(0, 1, 0, GRB_BINARY);
+			lvars[i].push_back(var);
+		}
+	}
+	model->update();
+	return 0;
+}
+
+int ilpsolver::add_upper_endpoints_variables()
+{
+	uvars.clear();
+	uvars.resize(spectrum.size());
+	for(int i = 0; i < spectrum.size(); i++)
+	{
+		for(int k = 0; k < slots; k++)
+		{
+			GRBVar var = model->addVar(0, 1, 0, GRB_BINARY);
+			uvars[i].push_back(var);
+		}
+	}
+	model->update();
+	return 0;
+}
+
+int ilpsolver::add_range_variables()
+{
+	rvars.clear();
+	rvars.resize(slots);
+	for(int i = 0; i < slots; i++)
+	{
+		for(int k = 0; k < slots; k++)
+		{
+			GRBVar var = model->addVar(0, ubound, 0, GRB_CONTINUOUS);
+			rvars[i].push_back(var);
+		}
+	}
+	model->update();
+	return 0;
+}
+
+int ilpsolver::add_error_variables()
+{
+	evars.clear();
+	for(int i = 0; i < spectrum.size(); i++)
+	{
+		GRBVar var = model->addVar(0, ubound, 0, GRB_CONTINUOUS);
+		evars.push_back(var);
+	}
+	model->update();
+	return 0;
+}
+
 int ilpsolver::print()
 {
+	/*
 	for(MSD::iterator it = aa2m.begin(); it != aa2m.end(); it++)
 	{
 		string s = it->first;
 		double m = it->second;
 		printf("amino acid %s -> %.2lf\n", s.c_str(), m);
 	}
-	printf("number of amino acid = %d\n", num_aa);
+	*/
+	assert(aa_list.size() == aa_mass.size());
+	for(int i = 0; i < aa_list.size(); i++)
+	{
+		printf("amino acid %d: %s -> %.2lf\n", i, aa_list[i].c_str(), aa_mass[i]);
+	}
+	printf("number of amino acid = %d\n", slots);
 	for(int i = 0; i < spectrum.size(); i++)
 	{
 		printf("spectrum: %.2lf\n", spectrum[i]);
--- ilpsolver/src/ilpsolver.h
+++ ilpsolver/src/ilpsolver.h
@@ -18,23 +18,39 @@ public:
 	~ilpsolver();
 
 public:
-	MSD aa2m;
-	int num_aa;
+	// instance
+	int slots;
+	double ubound;
 	vector<double> spectrum;
+	vector<string> aa_list;
+	vector<double> aa_mass;
 
 	// members for ILP
 	GRBModel * model;
 	GRBEnv * env;
 
-	vector<GRBVar> vars;					// gene variables
+	vector< vector<GRBVar> > xvars;		// amino acid variables
+	vector< vector<GRBVar> > lvars;		// lower endpoints variables
+	vector< vector<GRBVar> > uvars;		// upper endpoints variables
+	vector< vector<GRBVar> > rvars;		// range variables
+	vector<GRBVar> evars;				// error variables
 
 public:
 	int solve();
 	int print();
 
 private:
+	// read input files
 	int read_alphabet(const string &file);
 	int read_spectrum(const string &file);
+
+	// add variables
+	int add_amino_acid_variables();
+	int add_lower_endpoints_variables();
+	int add_upper_endpoints_variables();
+	int add_range_variables();
+	int add_error_variables();
+
 };
 
 #endif
