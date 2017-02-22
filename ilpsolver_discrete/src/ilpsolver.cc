e1ee94a556ddd3719f27be536a944b8924531d79
Mingfu Shao <shaomingfu@gmail.com>
2017-02-21 20:45:05 -0500

    create breach belt

--- ilpsolver_discrete/src/ilpsolver.cc
+++ ilpsolver_discrete/src/ilpsolver.cc
@@ -27,28 +27,13 @@ int ilpsolver::solve()
 		add_amino_acid_variables();
 		add_lower_endpoints_variables();
 		add_upper_endpoints_variables();
-		if(no_infinity_contraints){
-			add_set_map_variables();
-			add_set_acid_map_variables();		
-		}else{
-			add_range_variables();
-		}
-
+		add_range_variables();
 		add_error_variables();
 		
 		add_amino_acid_constraints();
 		add_lower_endpoints_constraints();
 		add_upper_endpoints_constraints();
-		if(no_infinity_contraints){
-			add_set_map_lbound_constraints();
-			add_set_map_ubound_constraints();
-			add_set_map_constraints();
-			add_set_acid_map_constraints();
-			add_set_location_map_ubound_constraints();
-			add_set_location_map_lbound_constraints();
-		}else{
-			add_range_constraints();
-		}
+		add_range_constraints();
 		add_error_constraints();
 		
 		set_objective();
@@ -56,8 +41,7 @@ int ilpsolver::solve()
 		model->getEnv().set(GRB_DoubleParam_TimeLimit, ilp_time_limit);
 
 		model->update();
-
-		model->write("temp.lp");
+		//model->write("temp.lp");
 
 		model->optimize();
 
@@ -191,35 +175,6 @@ int ilpsolver::add_upper_endpoints_variables()
 	return 0;
 }
 
-int ilpsolver::add_set_map_variables(){
-	ovars.clear();
-	ovars.resize(slots);
-	for(int i=0; i<slots; i++){
-		for(int p=0; p<spectrum.size();p++){
-			GRBVar var = model->addVar(0,1,0,GRB_BINARY);
-			ovars[i].push_back(var);
-		}
-	}
-	model->update();
-	return 0;
-}
-
-int ilpsolver::add_set_acid_map_variables(){
-	svars.clear();
-	svars.resize(slots);
-	for(int i=0;i<slots;i++){
-		svars[i].clear();
-		svars[i].resize(aa_list.size());
-		for(int j=0;j<aa_list.size();j++){
-			for(int p=0;p<spectrum.size();p++){
-				GRBVar var = model->addVar(0,1,0,GRB_BINARY);
-				svars[i][j].push_back(var);
-			}
-		}
-	}
-	return 0;
-}
-
 int ilpsolver::add_range_variables()
 {
 	rvars.clear();
@@ -290,102 +245,6 @@ int ilpsolver::add_upper_endpoints_constraints()
 	return 0;
 }
 
-int ilpsolver::add_set_map_lbound_constraints(){
-	for(int k=0;k<slots;k++){
-		for(int l=0;l<slots;l++){
-			for(int p=0;p<spectrum.size();p++){
-				GRBLinExpr expr = lvars[p][k] + uvars[p][l] - 1;
-				if(k<=l){
-					for(int i=k;i<=l;i++){
-						model->addConstr(ovars[i][p], GRB_GREATER_EQUAL, expr);
-					}
-				}else{
-					for(int i=k;i<slots;i++){
-						model->addConstr(ovars[i][p], GRB_GREATER_EQUAL, expr);
-					}
-					for(int i=0;i<=l;i++){
-						model->addConstr(ovars[i][p], GRB_GREATER_EQUAL, expr);
-					}
-				}
-			}
-		}
-	}
-	return 0;
-}
-
-int ilpsolver::add_set_map_ubound_constraints(){
-        for(int k=0;k<slots;k++){
-                for(int l=0;l<slots;l++){
-                        for(int p=0;p<spectrum.size();p++){
-                                GRBLinExpr expr = 2 - lvars[p][k] - uvars[p][l];
-                                if((l+1)%slots!=k){
-					if(l<k){
-                                	        for(int i=l+1;i<k;i++){
-                        	                        model->addConstr(ovars[i][p], GRB_LESS_EQUAL, expr);
-                	                        }
-        	                        }else{
-	                                        for(int i=l+1;i<slots;i++){
-                                        	        model->addConstr(ovars[i][p], GRB_LESS_EQUAL, expr);
-                                	        }
-                        	                for(int i=0;i<k;i++){
-                	                                model->addConstr(ovars[i][p], GRB_LESS_EQUAL, expr);
-        	                                }
-	                                }
-				}
-                        }
-                }
-        }
-        return 0;
-}
-
-int ilpsolver::add_set_acid_map_constraints(){
-	for(int i=0;i<slots;i++){
-		for(int j=0;j<aa_list.size();j++){
-			for(int p=0;p<spectrum.size();p++){
-				model->addConstr(svars[i][j][p], GRB_LESS_EQUAL, xvars[i][j]);
-			}
-		}
-	}
-	return 0;
-}
-
-int ilpsolver::add_set_location_map_ubound_constraints(){
-        for(int i=0;i<slots;i++){
-                for(int j=0;j<aa_list.size();j++){
-                        for(int p=0;p<spectrum.size();p++){
-                                model->addConstr(svars[i][j][p], GRB_LESS_EQUAL, ovars[i][p]);
-                        }
-                }
-        }
-        return 0;
-}
-
-int ilpsolver::add_set_location_map_lbound_constraints(){
-        for(int i=0;i<slots;i++){
-        	for(int p=0;p<spectrum.size();p++){
-			GRBLinExpr expr;
-			for(int j=0;j<aa_list.size();j++){
-				expr += svars[i][j][p];
-                        }
-			model->addConstr(expr, GRB_GREATER_EQUAL, ovars[i][p]);
-                }
-        }
-        return 0;
-}
-
-int ilpsolver::add_set_map_constraints(){
-	for(int p=0;p<spectrum.size();p++){
-		GRBLinExpr expr;
-		for(int i=0;i<slots;i++){
-			for(int j=0;j<aa_list.size();j++){
-				expr += svars[i][j][p];
-			}
-		}
-		model->addConstr(expr, GRB_GREATER_EQUAL, 1);
-	}
-	return 0;
-}
-
 int ilpsolver::add_range_constraints()
 {
 	for(int k = 0; k < slots; k++)
@@ -428,35 +287,19 @@ int ilpsolver::add_range_constraints()
 
 int ilpsolver::add_error_constraints()
 {
-	if(no_infinity_contraints){
-		for(int p=0;p<spectrum.size();p++){
-			GRBLinExpr expr1 = spectrum[p];
-                        GRBLinExpr expr2;
-			for(int i=0;i<slots;i++){
-				for(int j=0;j<aa_list.size();j++){
-					expr1 -= svars[i][j][p] * aa_mass[j];
-					expr2 += svars[i][j][p] * aa_mass[j];
-				}
-			}
-                	expr2 -= spectrum[p];
-			model->addConstr(evars[p], GRB_GREATER_EQUAL, expr1);
-                        model->addConstr(evars[p], GRB_GREATER_EQUAL, expr2);
-		}
-	}else{
-		for(int p = 0; p < spectrum.size(); p++)
+	for(int p = 0; p < spectrum.size(); p++)
+	{
+		for(int k = 0; k < slots; k++)
 		{
-			for(int k = 0; k < slots; k++)
+			for(int l = 0; l < slots; l++)
 			{
-				for(int l = 0; l < slots; l++)
-                        	{
-					double bound = 0;
-					if(k <= l) bound = (l - k + 1) * max_weight;
-					else bound = (l + 1 + slots - k) * max_weight;
-					GRBLinExpr expr1 = rvars[k][l] - spectrum[p] + bound * (lvars[p][k] + uvars[p][l] - 2);
-					GRBLinExpr expr2 = spectrum[p] - rvars[k][l] + bound * (lvars[p][k] + uvars[p][l] - 2);
-					model->addConstr(evars[p], GRB_GREATER_EQUAL, expr1);
-					model->addConstr(evars[p], GRB_GREATER_EQUAL, expr2);
-				}
+				double bound = 0;
+				if(k <= l) bound = (l - k + 1) * max_weight;
+				else bound = (l + 1 + slots - k) * max_weight;
+				GRBLinExpr expr1 = rvars[k][l] - spectrum[p] + bound * (lvars[p][k] + uvars[p][l] - 2);
+				GRBLinExpr expr2 = spectrum[p] - rvars[k][l] + bound * (lvars[p][k] + uvars[p][l] - 2);
+				model->addConstr(evars[p], GRB_GREATER_EQUAL, expr1);
+				model->addConstr(evars[p], GRB_GREATER_EQUAL, expr2);
 			}
 		}
 	}
@@ -528,22 +371,6 @@ int ilpsolver::collect_results()
 		xassign.push_back(k);
 	}
 
-	if(no_infinity_contraints){
-		sjassign.clear();
-		sjassign.resize(spectrum.size());
-		siassign.clear();
-		siassign.resize(spectrum.size());
-		for(int p = 0; p < spectrum.size(); p++){
-			for(int j = 0; j < aa_list.size(); j++){
-				for(int i = 0; i < slots; i++){
-					if(svars[i][j][p].get(GRB_DoubleAttr_X) <= 0.5) continue;
-					sjassign[p].push_back(j);
-					siassign[p].push_back(i);
-				}
-			}
-		}
-	}
-
 	lassign.clear();
 	for(int p = 0; p < spectrum.size(); p++)
 	{
@@ -572,17 +399,15 @@ int ilpsolver::collect_results()
 		uassign.push_back(k);
 	}
 
-	if(!no_infinity_contraints){
-		wassign.clear();
-		for(int p = 0; p < spectrum.size(); p++)
-		{
-			int l = lassign[p];
-			int u = uassign[p];
-			assert(l >= 0 && l < slots);
-			assert(u >= 0 && u < slots);
-			double w = rvars[l][u].get(GRB_DoubleAttr_X);
-			wassign.push_back(w);
-		}
+	wassign.clear();
+	for(int p = 0; p < spectrum.size(); p++)
+	{
+		int l = lassign[p];
+		int u = uassign[p];
+		assert(l >= 0 && l < slots);
+		assert(u >= 0 && u < slots);
+		double w = rvars[l][u].get(GRB_DoubleAttr_X);
+		wassign.push_back(w);
 	}
 
 	eassign.clear();
@@ -626,39 +451,11 @@ int ilpsolver::print()
 	{
 		int l = lassign[i];
 		int u = uassign[i];
-		double w = 0;
-		if(no_infinity_contraints){
-			if(l<=u){
-				for(int i=l;i<=u;i++) w += aa_mass[xassign[i]];
-			}else{
-				for(int i=l;i<slots;i++) w += aa_mass[xassign[i]];
-				for(int i=0;i<=u;i++) w += aa_mass[xassign[i]];
-			}
-		}else w = wassign[i];
+		double w = wassign[i];
 		double e = eassign[i];
-		printf("spectrum %d with mass %.3lf is assigned to interval [%d, %d], with mass %.3lf and error %.3lf (actual %.3lf)\n", i, spectrum[i], l, u, w, e, (w-spectrum[i]));
+		printf("spectrum %d with mass %.3lf is assigned to interval [%d, %d], with mass %.3lf and error %.3lf (actual %.3lf)\n", 
+				i, spectrum[i], l, u, w, e, (w-spectrum[i]));
 		
-		if(false && no_infinity_contraints){
-			printf("\t");
-			double sum = 0;
-			for(int c = 0; c<sjassign[i].size(); c++){
-				int j = sjassign[i][c];
-				if(c!=0) printf(",");
-				printf("%d (%s)",j, aa_list[j].c_str());
-				sum += aa_mass[j];
-			}
-			printf(" = %.3lf\n",sum);
-
-			sum = 0;
-			printf("\t");
-                        for(int c = 0; c<siassign[i].size(); c++){
-                                int j = siassign[i][c];
-                                if(c!=0) printf(",");
-                                printf("%d (%s)",j, aa_list[xassign[j]].c_str());
-                                sum += aa_mass[xassign[j]];
-                        }
-                        printf(" = %.3lf\n",sum);
-		}
 	}
 	return 0;
 }
--- ilpsolver_discrete/src/ilpsolver.h
+++ ilpsolver_discrete/src/ilpsolver.h
@@ -34,8 +34,6 @@ public:
 	vector< vector<GRBVar> > lvars;		// lower endpoints variables
 	vector< vector<GRBVar> > uvars;		// upper endpoints variables
 	vector< vector<GRBVar> > rvars;		// range variables
-	vector< vector<GRBVar> > ovars;		// map location  variables
-	vector< vector< vector<GRBVar> > > svars;		// map location acit variables
 	vector<GRBVar> evars;				// error variables
 
 	// results
@@ -44,8 +42,6 @@ public:
 	vector<int> uassign;
 	vector<double> wassign;
 	vector<double> eassign;
-	vector< vector<int> > sjassign;
-	vector< vector<int> > siassign;
 	
 public:
 	int solve();
@@ -64,9 +60,6 @@ private:
 	int add_upper_endpoints_variables();
 	int add_range_variables();
 	int add_error_variables();
-	int add_set_map_variables();
-        int add_set_acid_map_variables();
-
 
 	// add constraints
 	int add_amino_acid_constraints();
@@ -74,12 +67,6 @@ private:
 	int add_upper_endpoints_constraints();
 	int add_range_constraints();
 	int add_error_constraints();
-        int add_set_map_lbound_constraints();
-        int add_set_map_ubound_constraints();
-        int add_set_map_constraints();
-        int add_set_acid_map_constraints();
-        int add_set_location_map_ubound_constraints();
-        int add_set_location_map_lbound_constraints();
 
 	// cutting planes (extra constraints)
 	int add_ordering_cutting_planes();
